bdf8ba982a6608bc6b4f43de892860075dba2a89
Wohesi <woutsingerling@gmail.com>
2017-11-14 13:54:41 +0100

    added ukriane to classes

--- country.py
+++ country.py
@@ -5,5 +5,17 @@ import matplotlib.pyplot as plt
 class country:
 
     def __init__(self, name, csvfile):
-        cg = nx.graph()
-        cg.add_nodes_from()
+        #print(csvfile)
+        with open(csvfile) as csvfile:
+            countryFile = csv.reader(csvfile)
+            provincePairs = [pair for pair in countryFile]
+            print(provincePairs)
+            #countryWithBorders = [pair for pair in countryFile][1:]
+            #provinceNames = lit(set(x[0] for x in countryWithBorders))
+
+
+        #cg = nx.graph()
+        #cg.add_nodes_from(csvfile)
+
+
+USA  = country("USA", "csv-borders/UkraineCompleteDataset.csv")
