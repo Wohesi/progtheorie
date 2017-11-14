ba66502f119c6f2108f841ed37632975821fa738
Wohesi <woutsingerling@gmail.com>
2017-11-14 15:36:47 +0100

    visualisatie ambt en bij behordende testen

--- country.py
+++ country.py
@@ -4,34 +4,36 @@ import matplotlib.pyplot as plt
 
 class country:
 
+    cg = nx.Graph()
+
     def __init__(self, name, csvfile):
         #print(csvfile)
         with open(csvfile, encoding="utf8") as csvfile:
             countryFile = csv.reader(csvfile)
             provincePairs = [pair for pair in countryFile][1:]
-            #print(provincePairs)
             provinceNames = list(set(x[0] for x in provincePairs))
-            print (len(provinceNames))
-            #print (provinceNames)
 
-        cg = nx.Graph()
-        cg.add_nodes_from(provinceNames)
-        cg.add_edges_from(provincePairs)
+        # adding names and pairs to Graph
+        self.cg = nx.Graph()
+        self.cg.add_nodes_from(provinceNames)
+        self.cg.add_edges_from(provincePairs)
 
-        nx.spring_layout(cg)
+
+    def visualisation(self):
+        nx.spring_layout(self.cg)
 
         plt.figure(figsize=(15,15))
 
-        pos = nx.spring_layout(cg)
-        nx.draw_networkx_nodes(cg, pos,
-                                nodelist=cg.nodes(),
+        pos = nx.spring_layout(self.cg)
+        nx.draw_networkx_nodes(self.cg, pos,
+                                nodelist=self.cg.nodes(),
                                 node_color="yellow",
                                 alpha=0.4)
-        nx.draw_networkx_labels(cg, pos,
-                                nodelist=cg.nodes(),
+        nx.draw_networkx_labels(self.cg, pos,
+                                nodelist=self.cg.nodes(),
                                 font_size=14)
-        nx.draw_networkx_edges(cg, pos,
-                                edgelist=cg.edges(),
+        nx.draw_networkx_edges(self.cg, pos,
+                                edgelist=self.cg.edges(),
                                 edge_color="grey")
         plt.axis('off')
         plt.show()
@@ -40,3 +42,8 @@ UA  = country("Ukraine", "csv-borders/UkraineCompleteDataset.csv")
 USA  = country("United States of America", "csv-borders/USACompleteDataset.csv")
 RU  = country("Russia", "csv-borders/RussiaCompleteDataset.csv")
 CN  = country("China", "csv-borders/ChinaCompleteDataset.csv")
+
+UA.visualisation()
+USA.visualisation()
+RU.visualisation()
+CN.visualisation()
