d9d18ae6d24b4375362a3d42502e4b4839e5bc50
Wohesi <woutsingerling@gmail.com>
2017-11-14 15:06:33 +0100

    added edges

--- country.py
+++ country.py
@@ -9,14 +9,14 @@ class country:
         with open(csvfile, encoding="utf8") as csvfile:
             countryFile = csv.reader(csvfile)
             provincePairs = [pair for pair in countryFile][1:]
-            print(provincePairs)
+            #print(provincePairs)
             provinceNames = list(set(x[0] for x in provincePairs))
-            #print (len(provinceNames))
+            print (len(provinceNames))
             #print (provinceNames)
 
-        #cg = nx.graph()
-        #cg.add_nodes_from(provinceNames)
-
+        cg = nx.Graph()
+        cg.add_nodes_from(provinceNames)
+        cg.add_edges_from(provincePairs)
 
 #UA  = country("Ukraine", "csv-borders/UkraineCompleteDataset.csv")
 USA  = country("United States of America", "csv-borders/USACompleteDataset.csv")
