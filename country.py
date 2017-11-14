775f608c432212f9db904c4879ff30db1fcf5471
Wohesi <woutsingerling@gmail.com>
2017-11-14 14:57:16 +0100

    moved files and updated the country class

--- country.py
+++ country.py
@@ -9,15 +9,16 @@ class country:
         with open(csvfile, encoding="utf8") as csvfile:
             countryFile = csv.reader(csvfile)
             provincePairs = [pair for pair in countryFile][1:]
-            #print(provincePairs)
+            print(provincePairs)
             provinceNames = list(set(x[0] for x in provincePairs))
-            print (len(provinceNames))
+            #print (len(provinceNames))
+            #print (provinceNames)
 
-        cg = nx.graph()
-        cg.add_nodes_from(provinceNames)
+        #cg = nx.graph()
+        #cg.add_nodes_from(provinceNames)
 
 
-UA  = country("Ukraine", "csv-borders/UkraineCompleteDataset.csv")
-#USA  = country("United States of America", "csv-borders/USACompleteDataset.csv")
+#UA  = country("Ukraine", "csv-borders/UkraineCompleteDataset.csv")
+USA  = country("United States of America", "csv-borders/USACompleteDataset.csv")
 #RU  = country("Russia", "csv-borders/RussiaCompleteDataset.csv")
 #CN  = country("China", "csv-borders/ChinaCompleteDataset.csv")
