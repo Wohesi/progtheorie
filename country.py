81722402f92e4765b6e735c4d82f5c485f01449a
Wohesi <woutsingerling@gmail.com>
2017-11-14 14:23:51 +0100

    making a country into graph

--- country.py
+++ country.py
@@ -8,17 +8,16 @@ class country:
         #print(csvfile)
         with open(csvfile, encoding="utf8") as csvfile:
             countryFile = csv.reader(csvfile)
-            provincePairs = [pair for pair in countryFile]
-            print(provincePairs)
-            #countryWithBorders = [pair for pair in countryFile][1:]
-            #provinceNames = lit(set(x[0] for x in countryWithBorders))
+            provincePairs = [pair for pair in countryFile][1:]
+            #print(provincePairs)
+            provinceNames = list(set(x[0] for x in provincePairs))
+            print (len(provinceNames))
 
-
-        #cg = nx.graph()
-        #cg.add_nodes_from(csvfile)
+        cg = nx.graph()
+        cg.add_nodes_from(provinceNames)
 
 
 UA  = country("Ukraine", "csv-borders/UkraineCompleteDataset.csv")
-USA  = country("United States of America", "csv-borders/USACompleteDataset.csv")
-RU  = country("Russia", "csv-borders/RussiaCompleteDataset.csv")
-CN  = country("China", "csv-borders/ChinaCompleteDataset.csv")
+#USA  = country("United States of America", "csv-borders/USACompleteDataset.csv")
+#RU  = country("Russia", "csv-borders/RussiaCompleteDataset.csv")
+#CN  = country("China", "csv-borders/ChinaCompleteDataset.csv")
