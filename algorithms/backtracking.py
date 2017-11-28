7c3e18990d517b4e4c194e6f47bc788a3996e732
Wohesi <woutsingerling@gmail.com>
2017-11-28 15:18:34 +0100

    initial file for backtracking algorithm

---
+++ algorithms/backtracking.py
@@ -0,0 +1,10 @@
+import networkx as nx
+import matplotlib.pyplot as pyplot
+
+from algorithms import algorithm
+
+class backtracking(algorithm):
+    def __init__(self, graph):
+        '''
+        backtracking algoritme
+        '''
