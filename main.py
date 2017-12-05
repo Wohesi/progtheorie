df488dd9c9ccbef471c1a9186ef3ac1b73166b72
Mathijs Parmentier <mathijspar@gmail.com>
2017-12-05 15:51:06 +0100

    cleaned up main.py a bit

--- main.py
+++ main.py
@@ -2,84 +2,27 @@ import sys
 sys.path.insert(0,'algorithms')
 sys.path.insert(0,'bin')
 
-
-# from algorithms import greedy
-# from algorithms import DSatur
 from greedy import greedy
 from dsatur import DSatur
 from country import country
+from visualisation import visualisation
 
-'''
-def main(String dataSetName, String algorithmName, ):
-    if
+UA = country("Ukraine", "csv-borders/UkraineCompleteDataset.csv")
+USA = country("United States of America", "csv-borders/USACompleteDataset.csv")
+CN = country("China", "csv-borders/ChinaCompleteDataset.csv")
+RU = country("Russia", "csv-borders/NewRussiaCompleteDataset.csv")
 
-UA1  = country("Ukraine", "csv-borders/UkraineCompleteDataset.csv")
-# USA1  = country("United States of America", "csv-borders/USACompleteDataset.csv")
-# RU1  = country("Russia", "csv-borders/NewRussiaCompleteDataset.csv")
-# CN1  = country("China", "csv-borders/ChinaCompleteDataset.csv")
+greedy(UA, "degree")
+greedy(USA, "alphabetical")
+DSatur(CN)
+DSatur(RU)
 
-greedy(UA1.cg, "degree")
-# greedy(USA1.cg, "degree")
-# greedy(RU1.cg, "degree")
-# greedy(CN1.cg, "degree")
+UA1 = country("Ukraine", "csv-borders/UkraineCompleteDataset.csv")
+UA2 = country("Ukraine", "csv-borders/UkraineCompleteDataset.csv")
+UA3 = country("Ukraine", "csv-borders/UkraineCompleteDataset.csv")
 
-# UA2  = country("Ukraine", "csv-borders/UkraineCompleteDataset.csv")
-USA2  = country("United States of America", "csv-borders/USACompleteDataset.csv")
-# RU2  = country("Russia", "csv-borders/NewRussiaCompleteDataset.csv")
-# CN2  = country("China", "csv-borders/ChinaCompleteDataset.csv")
+greedy(UA1, "degree")
+greedy(UA2, "alphabetical")
+DSatur(UA3)
 
-# greedy(UA2.cg, "degree", True)
-greedy(USA2.cg, "degree", True)
-# greedy(RU2.cg, "degree", True)
-# greedy(CN2.cg, "degree", True)
-
-# UA3  = country("Ukraine", "csv-borders/UkraineCompleteDataset.csv")
-# USA3  = country("United States of America", "csv-borders/USACompleteDataset.csv")
-RU3  = country("Russia", "csv-borders/NewRussiaCompleteDataset.csv")
-# CN3  = country("China", "csv-borders/ChinaCompleteDataset.csv")
-
-# greedy(UA3.cg, "alphabetical")
-# greedy(USA3.cg, "alphabetical")
-greedy(RU3.cg, "alphabetical")
-# greedy(CN3.cg, "alphabetical")
-
-# UA4  = country("Ukraine", "csv-borders/UkraineCompleteDataset.csv")
-# USA4  = country("United States of America", "csv-borders/USACompleteDataset.csv")
-# RU4  = country("Russia", "csv-borders/NewRussiaCompleteDataset.csv")
-CN4  = country("China", "csv-borders/ChinaCompleteDataset.csv")
-
-# greedy(UA4.cg, "alphabetical", True)
-# greedy(USA4.cg, "alphabetical", True)
-# greedy(RU4.cg, "alphabetical", True)
-greedy(CN4.cg, "alphabetical", True)
-
-UA1.distribution()
-USA2.distribution()
-RU3.distribution()
-CN4.distribution()
-'''
-
-# UA = country("Ukraine", "csv-borders/UkraineCompleteDataset.csv")
-# DSatur(UA.cg)
-# USA = country("USA", "csv-borders/USACompleteDataset.csv")
-# DSatur(USA.cg)
-# RU  = country("Russia", "csv-borders/NewRussiaCompleteDataset.csv")
-# DSatur(RU.cg)
-CN4 = country("China", "csv-borders/USACompleteDataset.csv")
-greedy(CN4.cg, "degree", True)
-CN2 = country("China", "csv-borders/USACompleteDataset.csv")
-greedy(CN2.cg, "alphabetical")
-CN = country("China", "csv-borders/USACompleteDataset.csv")
-DSatur(CN.cg)
-# UA.distribution()
-# USA.distribution()
-# RU.distribution()
-
-CN4.distribution()
-CN2.distribution()
-CN.distribution()
-
-# UA.visualisation()
-# USA.visualisation()
-# RU.visualisation()
-# CN.visualisation()
+visualisation.distribution([UA1, UA2, UA3])
