6973c11f86a3726104d8daa4c3805b535773cc26
Mathijs Parmentier <mathijspar@gmail.com>
2017-12-12 14:09:10 +0100

    added a cost scheme calculator

--- country.py
+++ country.py
@@ -3,6 +3,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
+import itertools
 sys.path.insert(0,'algorithms')
 
 from collections import Counter
@@ -27,3 +28,36 @@ class country:
         self.cg = nx.Graph()
         self.cg.add_nodes_from(provinceNames, freq=None)
         self.cg.add_edges_from(provincePairs)
+
+    def getLowestCost(self, costScheme):
+        '''
+        costScheme : a list of 7 integers
+        '''
+        lowestCost = 999999999999999999999999
+        optimalScheme = []
+        permutations = list(itertools.permutations(costScheme))
+
+        for c in permutations:
+            if self.calcCost(c) < lowestCost:
+                lowestCost = self.calcCost(c)
+                optimalScheme = c
+
+
+        print("country name: " + self.countryName)
+        print("algorithm type: " + self.algorithmType)
+        print("lowest cost: " + str(lowestCost))
+        print("scheme used: " + str(optimalScheme))
+        print("")
+        return lowestCost
+
+    def calcCost(self, costScheme):
+        cost = 0
+        freqCounts = Counter(nx.get_node_attributes(self.cg, 'freq').values())
+
+        for i,c in enumerate(costScheme):
+            if i+1 in list(freqCounts.keys()):
+                cost += freqCounts[i+1] * c
+            else:
+                return cost
+
+        return cost
--- main.py
+++ main.py
@@ -7,6 +7,11 @@ from dsatur import DSatur
 from country import country
 from visualisation import visualisation
 
+costScheme1 = [12, 26, 27, 30, 37, 39, 41]
+costScheme2 = [19, 20, 21, 23, 36, 37, 38]
+costScheme3 = [16, 17, 31, 33, 36, 56, 57]
+costScheme4 = [3,  34, 36, 39, 41, 43, 58]
+
 UA = country("Ukraine", "csv-borders/UkraineCompleteDataset.csv")
 USA = country("United States of America", "csv-borders/USACompleteDataset.csv")
 CN = country("China", "csv-borders/ChinaCompleteDataset.csv")
@@ -17,12 +22,16 @@ greedy(USA, "alphabetical")
 DSatur(CN)
 DSatur(RU)
 
-UA1 = country("Ukraine", "csv-borders/UkraineCompleteDataset.csv")
-UA2 = country("Ukraine", "csv-borders/UkraineCompleteDataset.csv")
-UA3 = country("Ukraine", "csv-borders/UkraineCompleteDataset.csv")
-
-greedy(UA1, "degree")
-greedy(UA2, "alphabetical")
-DSatur(UA3)
-
-visualisation.distribution([UA1, UA2, UA3])
+RU.getLowestCost(costScheme1)
+RU.getLowestCost(costScheme2)
+RU.getLowestCost(costScheme3)
+RU.getLowestCost(costScheme4)
+# UA1 = country("Ukraine", "csv-borders/UkraineCompleteDataset.csv")
+# UA2 = country("Ukraine", "csv-borders/UkraineCompleteDataset.csv")
+# UA3 = country("Ukraine", "csv-borders/UkraineCompleteDataset.csv")
+#
+# greedy(UA1, "degree")
+# greedy(UA2, "alphabetical")
+# DSatur(UA3)
+#
+# visualisation.distribution([UA1, UA2])
