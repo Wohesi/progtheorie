4b1604527af9c27460f16f3049724215d2f558a9
Mathijs Parmentier <mathijspar@gmail.com>
2017-12-12 22:41:03 +0100

    different backtrack algorithm

---
+++ algorithms/bktrk.py
@@ -0,0 +1,53 @@
+import networkx as nx
+import copy
+
+from algorithms import algorithm
+
+import sys
+sys.path.insert(0,'algorithms')
+sys.path.insert(0,'bin')
+sys.setrecursionlimit(10000)
+
+class bktrk(algorithm):
+    # MIGHT NOT WORK DUE TO PYTHON RECURSION LIMITS
+
+    def __init__(self, country, maxFreq):
+        graph = country.cg
+        attrDict = nx.get_node_attributes(graph, 'freq')
+        attrList = [[x, attrDict[x]] for x in attrDict.keys()]
+        print(self.colorFirst(graph, attrList, maxFreq, 0))
+
+    def colorFirst(self, graph, attrList, maxFreq, n):
+        # WILL COLOUR IN THE FIRST NODE OF THE ATTRLIST
+
+        # WE CAN STOP IF THE GRAPH IS ALREADY COLOURED IN CORRECTLY
+        if algorithm.graphCheck(graph):
+            return attrList
+
+        print(n)
+        n += 1
+        newList = copy.deepcopy(attrList)
+
+        # COLOUR IN THE FIRST NODE
+        newList[0][1] = 1
+        graph.node[newList[0][0]]['freq'] = 1
+
+        # THE COLOURING CANT INTERFERE WITH IT'S NEIGHBOURS;
+        # LOOP UNTIL IT'S LEGAL
+        while not algorithm.neighborCheck(graph, newList[0][0]):
+            newList[0][1] += 1
+            graph.node[newList[0][0]]['freq'] += 1
+
+        # IF THE VALID COLOURING EXCEEDS THE MAXIMUM, WE GOTTA BACKTRACK
+        # PUT THE LAST NODE IN FRONT
+        if newList[0][1] > maxFreq:
+            newList[0][1] = None
+            graph.node[newList[0][0]]['freq'] = None
+
+            newerList = [newList[-1]] + newList[:-1]
+            return self.colorFirst(graph, newerList, maxFreq, n)
+
+        # IF IT DOESN'T EXCEED THE MAXIMUM, WE CAN PROCEED WITH THE NEXT NODE
+        else:
+            newerList = newList[1:] + [newList[0]]
+            return self.colorFirst(graph, newerList, maxFreq, n)
