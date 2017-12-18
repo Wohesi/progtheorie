fddc2b04249662099994eb918d878dd486de26d0
iliassrifie <iliass-@live.nl>
2017-12-18 16:23:30 +0100

    The advanced Assignment

--- advancedAssignment.py
+++ advancedAssignment.py
@@ -1,9 +1,10 @@
 #ADVANCED SECTION
 #a cost scheme that is variable in costs with
 #the numbers of frequencies of a type placed.
-
-schemeCustom = [5, 8, 13, 20, 25, 32, 38]
-count = [4, 2, 3, 5] # MOET VERANDERD WORDEN MET DE freqDict
+from collections import Counter
+from country import country
+from backtracking import backtracking
+import networkx as nx
 
 
 def customCostPercentage(x,z):
@@ -15,7 +16,7 @@ def customCostPercentage(x,z):
     if z == 0:
         return 0
     else:
-        return (x + customCostPercentage(x-0.90,z-1))
+        return (x + customCostPercentage(x*0.90,z-1))
 
 
 def customCostOneDollar(x,z):
@@ -30,21 +31,27 @@ def customCostOneDollar(x,z):
         return (x + customCostOneDollar(x-1,z-1))
 
 
-def advancedCostScheme(strategyType):
 
-     # gives the the final price of the custom scheme
-     #after the advanced calculations
-     cheapCost=sum([strategyType(j[0],j[1]) for j in list(zip(schemeCustom,count))])
+def advancedCostScheme(country,costScheme,strat):
 
-     if strategyType == customCostOneDollar:
-         print ("Costs by making the price every iteration 1 Dollar cheaper: "
-                + str(cheapCost))
-
-     if strategyType == customCostPercentage:
-         print ("Costs by making the price every iteration 10 percent cheaper: "
-         + str(cheapCost))
-     return
+    count = Counter(nx.get_node_attributes(country.cg, 'freq').values()) # MOET VERANDERD WORDEN MET DE freqDict
+    freqValues = list(count.values())
 
 
-advancedCostScheme(customCostPercentage)
-advancedCostScheme(customCostOneDollar)
+    strategyType = None
+
+    if strat == "customCostPercentage":
+            strategyType = customCostPercentage
+
+    if strat == "customCostOneDollar":
+            strategyType = customCostOneDollar
+
+    # gives the the final price of the custom scheme
+    #after the advanced calculations
+    cheapCost=sum([strategyType(j[0],j[1]) for j in list(zip(costScheme,freqValues))])
+
+    print("country name: " + country.countryName)
+    print("algorithm type: " + country.algorithmType)
+    print("lowest cost: " + str(cheapCost))
+    print("")
+    return cheapCost
